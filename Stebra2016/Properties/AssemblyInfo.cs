ac499addedcb000913ef8ed585a580f77e84d677
Administrator <Administrator@BATCORE2>
2016-09-08 11:27:29 +0200

    set copyright information in Assembly.cs

--- Stebra2016/Properties/AssemblyInfo.cs
+++ Stebra2016/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stebra2016")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The company's homepage")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Stebra2016")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Stebra Consulting AB, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
